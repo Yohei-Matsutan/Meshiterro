eab54c54fb75683fafab76080609b94935df290a
Yohei-Matsutan <takeda.yohei.sp@alumni.tsukuba.ac.jp>
2021-12-02 12:41:12 +0000

    [Finish]4-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # deviseのactionが実行される前にconfigure_permitted_parematersを実行するよ
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # privateだとこのcontroller内だけ、protectedだと他のcontrollerで呼び出されても参照される
+  protected
+  def configure_permitted_parameters
+    # ユーザ登録（sign_up）の際に、ユーザ名（name）のデータ操作を許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
