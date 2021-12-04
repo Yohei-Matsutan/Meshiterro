2b4e3b95decb1f67b4435035b707665e9b86b82e
Yohei-Matsutan <takeda.yohei.sp@alumni.tsukuba.ac.jp>
2021-12-04 11:34:13 +0000

    [Finsh]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
+  resources :users, only: [:show, :edit, :update]
+
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
     # resourceだと、idなしでパスが生成される(urlで指定した「いいねのid」をcontrollerで受け取る必要がないから)
