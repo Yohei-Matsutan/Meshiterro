4b2566e9804f1ea527b0524b41c53952d80fef78
Yohei-Matsutan <takeda.yohei.sp@alumni.tsukuba.ac.jp>
2021-12-08 05:44:52 +0000

    [Finish]4-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # deviseのactionが実行される前にconfigure_permitted_parematersを実行するよ
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # deviseのメソッド(authenticate_user!)を、コントローラが動作する前に実行する(before_action)
+  # exceptは例外
+  # ログインしていないユーザーはtop(新規登録かログインだけできる)に飛ばされる
+  before_action :authenticate_user!, except: [:top]
+
   def after_sign_in_path_for(resource)
     post_images_path
   end
@@ -9,11 +14,6 @@ class ApplicationController < ActionController::Base
   # privateだとこのcontroller内だけ、protectedだと他のcontrollerで呼び出されても参照される
   protected
   def configure_permitted_parameters
-    # deviseのメソッド(authenticate_user!)を、コントローラが動作する前に実行する(before_action)
-    # exceptは例外
-    # ログインしていないユーザーはtop(新規登録かログインだけできる)に飛ばされる
-    before_action :authenticate_user!,except: [:top]
-
     # ユーザ登録（sign_up）の際に、ユーザ名（name）のデータ操作を許可
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
